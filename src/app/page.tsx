5396a45ab9554b89f3ae9e8dd0f4a3307269be04
Priyabrata <priyabrataxofficial@gmail.com>
2025-08-28 12:33:51 +0530

    homepage mdified

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <>
+      <h1>Project -  # DOCX → HTML API</h1>
+      <div>
+        # DOCX → HTML API (Mammoth.js + Next.js) This is a simple API that
+        converts `.docx` documents into **clean HTML** using
+        [Mammoth.js](https://github.com/mwilliamson/mammoth.js). ## 🚀 Deploy on
+        Vercel 1. Fork this repo 2. Connect it to Vercel 3. Deploy → your API
+        will be live at:
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
